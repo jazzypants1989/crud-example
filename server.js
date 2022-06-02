24cc126ce0c22d18e9c203df44bf054cb89169cf
jazzypants1989 <jessepence@gmail.com>
2022-06-01 21:47:22 -0700

    what da fuck

---
+++ server.js
@@ -0,0 +1,34 @@
+console.log('May Node be with you')
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://jazzypants1989:Z0nkerswow@cluster0.ftizc.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+    console.log('Connected to database.')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.use(bodyParser.urlencoded({ extended: true}))
+
+    app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
+
+    app.post('/quotes',(req,res) => {
+    quotesCollection.insertOne(req.body)
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => console.log(error))
+    })
+
+    app.listen(3001, function(){
+        console.log('listening on 3001')
+    })
+    
+})
+    .catch(error => console.error(error))
